63316a1d195e61cfb0e467de265795b70e272c01
Simon Tucker <brotherlogic@gmail.com>
2018-09-10 17:25:17 -0700

    Added ability to process the write. This closes #5.

--- handler.go
+++ handler.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
-func handle(param string) string {
-	return fmt.Sprintf("Hi there, I love %s!", param)
+func (s *Server) handle(name, lat, lon, ip, port string) string {
+	latval, err := strconv.ParseFloat(lat, 32)
+	if err != nil {
+		return fmt.Sprintf("Error handling location update %v", err)
+	}
+	lonval, err := strconv.ParseFloat(lon, 32)
+	if err != nil {
+		return fmt.Sprintf("Error handling location update %v", err)
+	}
+
+	err = s.locwriter.writeLocation(name, float32(latval), float32(lonval), ip, port)
+	if err != nil {
+		return fmt.Sprintf("Error writing Location: %v", err)
+	}
+	return fmt.Sprintf("Location Written")
 }
--- server.go
+++ server.go
@@ -1,19 +1,56 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"google.golang.org/grpc"
+
+	pb "github.com/brotherlogic/location/proto"
 )
 
-func freebie() {
+type locwriter interface {
+	writeLocation(name string, lat, lon float32, ip, port string) error
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, handle(r.URL.Path[1:]))
+// Server main server type
+type Server struct {
+	ip        string
+	port      string
+	locwriter locwriter
+}
+
+type mainWriter struct{}
+
+func (m *mainWriter) writeLocation(name string, lat, lon float32, ip, port string) error {
+	conn, err := grpc.Dial(ip+":"+port, grpc.WithInsecure())
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	proxy := pb.NewLocationServiceClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	_, err = proxy.AddLocation(ctx, &pb.AddLocationRequest{Location: &pb.Location{Name: name, Lat: lat, Lon: lon, Time: time.Now().Unix()}})
+	return err
+}
+
+func (s *Server) handler(w http.ResponseWriter, r *http.Request) {
+	vals := strings.Split("/", r.URL.Path[1:])
+	if len(vals) != 3 {
+		fmt.Fprintf(w, "Can't handle request")
+	}
+	fmt.Fprintf(w, s.handle(vals[0], vals[1], vals[2], s.ip, s.port))
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	s := &Server{os.Args[1], os.Args[2], &mainWriter{}}
+	http.HandleFunc("/", s.handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
