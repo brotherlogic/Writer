b2b8c1c7948faa9d72ee8adfbcc020d6d09e1693
Simon Tucker <brotherlogic@gmail.com>
2018-09-13 23:31:20 -0700

    Fixes push script

--- push_script.sh
+++ push_script.sh
@@ -1,1 +1,3 @@
-export GOPATH=$(pwd) && go get -u github.com/brotherlogic/writer && env GOOS=linux GOARCH=amd64 go build && ssh $1 'killall writer' && scp writer $1: && ssh $1 'nohup ./writer &> out.txt &'
+export GOPATH=$(pwd) && go get -u github.com/brotherlogic/writer && env GOOS=linux GOARCH=amd64 go build
+ssh $1 'killall writer'
+scp writer $1: && ssh $1 'nohup ./writer &> out.txt &'
