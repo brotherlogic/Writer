9f857208e0382d1d28be980bdcd734cc76a4bf6a
Simon Tucker <brotherlogic@gmail.com>
2018-09-13 17:21:30 -0700

    Updated push script to return failure

--- push_script.sh
+++ push_script.sh
@@ -1,4 +1,1 @@
-env GOOS=linux GOARCH=amd64 go build
-ssh $1 'killall writer'
-scp writer $1:
-ssh $1 'nohup ./writer &> out.txt &'
+env GOOS=linux GOARCH=amd64 go build writer && ssh $1 'killall writer' && scp writer $1: && ssh $1 'nohup ./writer &> out.txt &'
