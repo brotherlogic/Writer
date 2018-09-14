a704c0dea864849a224ee6dccce60eddaed10c5f
Simon Tucker <brotherlogic@gmail.com>
2018-09-13 23:15:13 -0700

    Cleaning push script

--- push_script.sh
+++ push_script.sh
@@ -1,1 +1,1 @@
-export GOPATH=$(pwd) && env GOOS=linux GOARCH=amd64 go build writer && ssh $1 'killall writer' && scp writer $1: && ssh $1 'nohup ./writer &> out.txt &'
+export GOPATH=$(pwd) && go get -u github.com/brotherlogic/writer && env GOOS=linux GOARCH=amd64 go build && ssh $1 'killall writer' && scp writer $1: && ssh $1 'nohup ./writer &> out.txt &'
